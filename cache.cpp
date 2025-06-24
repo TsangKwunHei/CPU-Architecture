0c15553f3e717be3b3d6e41885e09675a2bfe369
TsangKwunHei <Tsangkwunhei@gmail.com>
2025-06-24 10:47:33 +0100

    cache.cpp

--- cache.cpp
+++ cache.cpp
@@ -1,33 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
 
-/*
-- simple logic direct map cahce, 
-input access order, 
-hit rate output
-- LRU
-
-*/
-/*
-Cache [
-cache_blocks[
-cache_entry [ 
-TAG
-Data_block
-flag_bits
-]
-]
-]
-num_bytes per block * num_block
-
-
-*/
 struct cache_entry {
     int tag;
-    int data_block;
-    bool flag_bits;
+    int data;
+    bool flagBits;
 }
 
-// 1 fetch from from memory 
-// flag_bits default to false
-// direct
+struct cache_entry cache_blocks[] = {
+    {12,123213213,false},
+    {12,123213213,false},
+    {12,123213213,false},
+    {12,123213213,false},
+};
 
-// 2 
+access_cache(int address)
+    bool hit
+    //index
+    int index = address % cache_blocks.size();
+    cache_now = cache_blocks[index];
+        //if tag match
+    if (cache_now.tag == address.tag) {
+        if  (cache_now.flagBits == true) {
+            //hit 
+            print(hit)
+            hit_count ++;
+        }
+    }
+    if (!hit) {
+        print(miss)
+        Direct_Map_cache(address);
+    }
+
+Direct_Map_cache(address) {
+    accessRAM(address);
+    int index = 1;
+    cache_entry.line[index] = address;
+}
+
+
+int main() {
+
+    // address, data
+    vector<tuple<int,int>> memory;
+    memory.pushback(make_tuple(10,10));
+
+    list< look_address[5] = {1,2,3,4,5};
+    for (addresss in look_address) {
+        access_cache(address)
+    }
+
+    cout << "hitRate =" << hit_count/RAM_address.size *100 << "%" ; 
+
+}
