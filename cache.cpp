331b6557ae334d1791bb65835da036e359226944
TsangKwunHei <Tsangkwunhei@gmail.com>
2025-06-24 11:13:34 +0100

    cache.cpp

--- cache.cpp
+++ cache.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include <tuple>
 
+int hit_count = 0;
 struct cache_entry {
     int tag;
     int data;
     bool flagBits;
-}
+};
 
 struct cache_entry cache_blocks[] = {
     {12,123213213,false},
@@ -15,29 +16,40 @@ struct cache_entry cache_blocks[] = {
     {12,123213213,false},
 };
 
-access_cache(int address)
-    bool hit
-    //index
+int accessRAM(int address) {
+    return 1;
+}
+
+void Direct_Map_cache(int address) {
+    int data_fetch = accessRAM(address);
     int index = address % cache_blocks.size();
-    cache_now = cache_blocks[index];
+    cache_entry cache_now = cache_blocks[index];
+    cache_now.tag = address[0-26]; //tag 26 bits
+    cache_now.data = data_fetch;
+
+}
+
+void access_cache(int address) {
+    bool hit = false;
+    //index
+    int index = address % sizeof(cache_blocks);
+    cache_entry cache_now = cache_blocks[index];
+    int address_tag = address >> 6 ;//tag 26 bits, drop 6 bits via shift
         //if tag match
-    if (cache_now.tag == address.tag) {
+    if (cache_now.tag == address_tag) {
         if  (cache_now.flagBits == true) {
             //hit 
-            print(hit)
+            std::cout << "miss";
             hit_count ++;
         }
     }
     if (!hit) {
-        print(miss)
+        std::cout << "miss";
         Direct_Map_cache(address);
     }
-
-Direct_Map_cache(address) {
-    accessRAM(address);
-    int index = 1;
-    cache_entry.line[index] = address;
 }
+
+
 
 
 int main() {
@@ -46,9 +58,15 @@ int main() {
     vector<tuple<int,int>> memory;
     memory.pushback(make_tuple(10,10));
 
-    list< look_address[5] = {1,2,3,4,5};
-    for (addresss in look_address) {
-        access_cache(address)
+    std::vector<int> look_address = {
+        0x00000000,   
+        0x00000010,  
+        0x00000040,   
+        0x00000001,  
+        0x00000101   
+        };
+    for (addresss : look_address) {
+        access_cache(address);
     }
 
     cout << "hitRate =" << hit_count/RAM_address.size *100 << "%" ; 
